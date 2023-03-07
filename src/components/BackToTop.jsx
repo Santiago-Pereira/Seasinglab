0977c86146b2a57f4b100346bd75b83f5ab13522
Santiago-Pereira <santiagopereira_8@hotmail.com>
2023-03-07 20:03:27 -0300

    "fixing classNames"

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -15,15 +15,15 @@ function BackToTop() {
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-chevron-double-up"
+          className="bi bi-chevron-double-up"
           viewBox="0 0 16 16"
         >
           <path
-            fill-rule="1evenodd"
+            fillRule="1evenodd"
             d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
           />
         </svg>
